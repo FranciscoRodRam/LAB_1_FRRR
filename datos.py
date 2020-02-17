ea505a607251fccd46abd46c9817b01fb0903da6
Francisco <franckrodriguez06@gmail.com>
2020-02-16 19:08:29 -0600

    Laboratorio_1

---
+++ datos.py
@@ -0,0 +1,8 @@
+# -- ------------------------------------------------------------------------------------ -- #
+# -- Proyecto: Repaso de python 3 y analisis de precios OHLC                              -- #
+# -- Codigo: datos.py - script con datos de uso en proyecto                               -- #
+# -- Rep: https://github.com/ITESOIF/MyST/tree/master/Notas_Python/Notas_RepasoPython     -- #
+# -- Autor: Francisco ME                                                                  -- #
+# -- ------------------------------------------------------------------------------------ -- #
+
+OA_Ak = '4' + '42de0e1239c7984ca4f78ed85957c84-9dfc793ef822512bb28ba0384553710' + '9'
